1ce07fbc625645dc6e09adb17cdf8fe3021bc97c
Jean Paul Cardenas <jeanpaul9712@gmail.com>
2025-05-31 00:07:37 +0200

    adjusted to function with new dash dropdown options argument {'label': 'my_label', 'value': 'my_value'}

--- application/dash_graph_f/income.py
+++ application/dash_graph_f/income.py
@@ -14,19 +14,20 @@ class IncomeGraph:
     def __init__(self, start: dt.datetime, end: dt.datetime, metrics_object: Metrics, choice: str):
         self.metrics_ob = metrics_object
         self.df = self.df_between_dates(start_date=start, end_date=end)
+        logger.info(f'{__name__} income graph df: {self.df}')
         self.choice = choice
 
     def df_between_dates(self, start_date: dt.datetime, end_date: dt.datetime) -> pd.DataFrame:
         df = self.metrics_ob.df
-        df_ranged = df[(df['open_time'] > start_date) & (df['close_time'] < end_date)].copy().reset_index(inplace=True)
-
+        print(f'metrics.df: \n {df}')
+        df_ranged = df[(df['open_time'] >= start_date) & (df['close_time'] <= end_date)].reset_index(drop=True)
         return df_ranged
 
     def layout(self) -> dict:
         return dict(template="plotly_dark",
                     showlegend=True,
                     title=dict(
-                        text="Ingreso acumulado", x=0.5,
+                        text='accumulative income', x=0.5,
                         font=dict(
                             color="#2C74B3",
                             family="sans-serif",
@@ -37,26 +38,27 @@ class IncomeGraph:
                     yaxis=dict(
                         griddash="solid",
                         zerolinecolor="rgba(120, 120, 120, 0.5)",
-                        ticksuffix=f" {self.metrics_ob.currency}",
+                        ticksuffix=self.metrics_ob.currency_symbol,
+                        tickformat='d',
+                        ticklabelposition='outside right',
                         separatethousands=True,
                     ))
 
-    def create_dataframes(self, choice: str) -> str:
+    def create_dataframes(self, choice: str) -> list[pd.DataFrame]:
         choices = {
-            'All': [self.df],
-            'Buy vs Sell': [self.df[self.df.order_type == order_type].copy().reset_index(inplace=True)
+            0: [self.df],
+            'order_type': [self.df[self.df.order_type == order_type].reset_index(drop=True)
                             for order_type in order_types],
-            'Pairs': [self.df[self.df.symbol == pair_symbol].copy().reset_index(inplace=True)
+            'symbol': [self.df[self.df.symbol == pair_symbol].reset_index(drop=True)
                       for pair_symbol in self.df['symbol'].unique()],
-            'Day of week': [self.df[self.df.day_of_week == day].copy().reset_index(inplace=True)
+            'day_of_week': [self.df[self.df.day_of_week == day].reset_index(drop=True)
                             for day in self.df['day_of_week'].unique()],
         }
         return choices[choice]
 
     def get_name(self, df):
-        if self.choice != "All":
-            column = COLUMN_USED[self.choice]
-            name = df[column][0]
+        if self.choice:
+            name = df[self.choice][0]
         else:
             name = "All trades"
         return name
@@ -66,29 +68,21 @@ class IncomeGraph:
         objs = self.create_dataframes(choice=self.choice)
         for i, df in enumerate(objs):
             name = self.get_name(df=df)
+            x = [df.open_time[0]] + df.close_time.to_list()
+            y = [0] + df.profit.cumsum().to_list()
             fig.add_trace(go.Scatter(
                 name=name,
                 showlegend=True,
                 legendgroup=name,
-                x=[df.open_time[0]] + df.close_time.to_list(),
-                y=[0] + df.profit.cumsum().to_list(),
-                mode="lines+markers",
+                x=x,
+                y=y,
+                mode='lines+markers+text',
+                textposition='top right',
+                texttemplate='%{y}',
                 line=dict(
                     width=2,
                     shape="hv",
                     color=list(COLORS.values())[i]
                 )))
-            last_data = fig.data[2 * i]
 
-            fig.add_scatter(
-                x=[last_data.x[-1]],
-                y=[last_data.y[-1]],
-                mode="text",
-                text="  {:,} {}".format(fig.data[2 * i].y[-1], self.metrics_ob.currency),
-                textfont=dict(color="white"),
-                textposition="middle right",
-                legendgroup=last_data["name"],
-                showlegend=False,
-
-            )
         return fig
