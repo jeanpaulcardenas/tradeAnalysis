9a14269c3a657b07be0739ab252a370a76a01289
Jean Paul Cardenas <jeanpaul9712@gmail.com>
2025-05-30 21:31:11 +0200

    logging: added logs for date range picker

--- application/dash_apps/graphs.py
+++ application/dash_apps/graphs.py
@@ -1,5 +1,6 @@
 from dash import dash, dcc, html, callback
 from dash.dependencies import Input, Output
+from application.config import get_logger
 from application.statistics import Metrics
 from application.dash_graph_f.income import IncomeGraph
 from application.random_df_generator import RandDataGen
@@ -7,20 +8,22 @@ import pandas as pd
 import datetime as dt
 
 
-my_simulated_df = RandDataGen(10, 'USD').df
-print(my_simulated_df.to_string())
 app = dash.Dash()
-metrics_obj = Metrics(my_simulated_df, pd.DataFrame(), 'USD')  # currency choices should be restricted or checked
-
+logger = get_logger(__name__)
+df = RandDataGen(80, 'USD').df
 try:
-    start_date = min(metrics_obj.df.close_time)
-    end_date = max(metrics_obj.df.close_time)
+    start_date = min(df.close_time)
+    end_date = max(df.close_time)
+    logger.info(f"{__name__} dates from date range picker: {start_date} to {end_date}")
 
-except:
+except Exception as e:
+    logger.error(f"Error getting dataframes from min and max dates of {df['close_time']}, "
+                 f"using 100 days back from today")
     now = dt.datetime.now()
-    start_date = now - dt.timedelta(days=10)
+    start_date = now - dt.timedelta(days=100)
     end_date = now
 
+metrics_obj = Metrics(df, pd.DataFrame(), 'USD')  # currency choices should be restricted or checked
 app.layout = html.Div([
     html.H1("Profit", style={"text-align": "center"}),
     html.Br(),
@@ -49,7 +52,7 @@ app.layout = html.Div([
      Input("date range", "end_date"),
      Input("income dropdown", "value")])
 def update_charts(start, end, choice):
-    income_graph = IncomeGraph(metrics_obj, choice=choice).get_figure()
+    income_graph = IncomeGraph(metrics_object=metrics_obj, start=start, end=end, choice=choice).get_figure()
     return [income_graph]
 
 
