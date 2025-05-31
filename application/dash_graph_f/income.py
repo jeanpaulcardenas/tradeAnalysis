bcb0f974510c14f8732bfa4338b0cc5c8170a0b3
Jean Paul Cardenas <jeanpaul9712@gmail.com>
2025-05-31 19:19:23 +0200

    Modularity: IncomeGraph does not filter by dates
    marker labels removed from go.scatter ('lines+markers')

--- application/dash_graph_f/income.py
+++ application/dash_graph_f/income.py
@@ -3,25 +3,17 @@ from application.constants import *
 from application.config import get_logger
 import pandas as pd
 import plotly.graph_objects as go
-import datetime as dt
-
 
 logger = get_logger(__name__)
 
 
 class IncomeGraph:
 
-    def __init__(self, start: dt.datetime, end: dt.datetime, metrics_object: Metrics, choice: str):
+    def __init__(self, metrics_object: Metrics, choice: str):
         self.metrics_ob = metrics_object
-        self.df = self.df_between_dates(start_date=start, end_date=end)
-        logger.info(f'{__name__} income graph df: {self.df}')
+        self.df = self.metrics_ob.df
+        logger.info(f'{__name__} income graph df: {self.df.head()}')
         self.choice = choice
-
-    def df_between_dates(self, start_date: dt.datetime, end_date: dt.datetime) -> pd.DataFrame:
-        df = self.metrics_ob.df
-        print(f'metrics.df: \n {df}')
-        df_ranged = df[(df['open_time'] >= start_date) & (df['close_time'] <= end_date)].reset_index(drop=True)
-        return df_ranged
 
     def layout(self) -> dict:
         return dict(template="plotly_dark",
@@ -48,9 +40,9 @@ class IncomeGraph:
         choices = {
             0: [self.df],
             'order_type': [self.df[self.df.order_type == order_type].reset_index(drop=True)
-                            for order_type in order_types],
+                           for order_type in order_types],
             'symbol': [self.df[self.df.symbol == pair_symbol].reset_index(drop=True)
-                      for pair_symbol in self.df['symbol'].unique()],
+                       for pair_symbol in self.df['symbol'].unique()],
             'day_of_week': [self.df[self.df.day_of_week == day].reset_index(drop=True)
                             for day in self.df['day_of_week'].unique()],
         }
@@ -76,7 +68,7 @@ class IncomeGraph:
                 legendgroup=name,
                 x=x,
                 y=y,
-                mode='lines+markers+text',
+                mode='lines+markers',
                 textposition='top right',
                 texttemplate='%{y}',
                 line=dict(
