e5a8f8056811aa27ecac431970fd7c947bde86e4
Jean Paul Cardenas <jeanpaul9712@gmail.com>
2025-06-07 00:06:13 +0200

    time graph added. IncomeGraph refactored

--- application/dash_apps/graphs.py
+++ application/dash_apps/graphs.py
@@ -2,7 +2,7 @@ from dash import dash, dcc, html, callback
 from dash.dependencies import Input, Output
 from application.config import get_logger
 from application.statistics import Metrics
-from application.dash_graph_f.income import IncomeGraph, BarGraph, SunBurst
+from application.dash_graph_f.income import ScatterGraph, BarGraph, SunBurst, TimeOpenIncome
 from application.random_df_generator import RandDataGen
 from application.constants import INCOME_DROPDOWN_OPTIONS, BARS_DROPDOWN_OPTIONS, METRICS_DROPDOWN_OPTIONS
 from application.helpers import metrics_between_dates
@@ -41,7 +41,7 @@ def app_layout(start_date, end_date):
         html.Br(),
         dcc.Dropdown(
             options=METRICS_DROPDOWN_OPTIONS,
-            value='profit',
+            value=False,
             id='metric dropdown'
         ),
         dcc.DatePickerRange(
@@ -69,7 +69,9 @@ def app_layout(start_date, end_date):
         ),
         dcc.Graph(id='bars graph'),
         html.Br(),
-        dcc.Graph(id='sunburst')
+        dcc.Graph(id='sunburst'),
+        html.Br(),
+        dcc.Graph(id='time graph')
     ])
 
     return layout
@@ -78,18 +80,20 @@ def app_layout(start_date, end_date):
 @callback(
     [Output('income graph', 'figure'),
      Output('bars graph', 'figure'),
-     Output('sunburst', 'figure')],
+     Output('sunburst', 'figure'),
+     Output('time graph', 'figure')],
     [Input('metric dropdown', 'value'),
      Input('date range', 'start_date'),
      Input('date range', 'end_date'),
      Input('income dropdown', 'value'),
      Input('bars dropdown', 'value')])
-def update_charts(metric, start_date, end_date, inc_choice, bars_choice):
+def update_charts(measure, start_date, end_date, inc_choice, bars_choice):
     metrics_obj = metrics_between_dates(random_metric, start_date=start_date, end_date=end_date)
-    income_graph = IncomeGraph(metrics_object=metrics_obj, choice=inc_choice).get_figure(metric)
-    bars_graph = BarGraph(metrics_obj=metrics_obj, choice=inc_choice, period=bars_choice).add_bar_plot()
+    income_graph = ScatterGraph(metrics_obj=metrics_obj, choice=inc_choice, pips=measure, title='Cumulative Income').get_figure()
+    bars_graph = BarGraph(metrics_obj=metrics_obj, choice=inc_choice, period=bars_choice).get_figure()
     sunburst = SunBurst(metrics_obj).get_figure()
-    return [income_graph, bars_graph, sunburst]
+    time_graph = TimeOpenIncome(metrics_obj, inc_choice, measure, title='Time open vs Income').get_figure()
+    return [income_graph, bars_graph, sunburst, time_graph]
 
 
 if __name__ == '__main__':
