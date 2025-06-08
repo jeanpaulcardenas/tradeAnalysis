f66d6ec548d929154ed0925d356243e9e0bfb729
Jean Paul Cardenas <jeanpaul9712@gmail.com>
2025-06-08 13:06:29 +0200

    refactored: new file paths

--- application/data/mt4data.py
+++ application/data/mt4data.py
@@ -550,7 +550,7 @@ class TraderMadeClient:
 
 
 if __name__ == '__main__':
-    aux = TxtParser.from_filepath('statement.txt')
+    aux = TxtParser.from_filepath('../statement.txt')
     operations_infonow = aux.get_operations_info()
     now = TradeData(aux)
 
@@ -559,5 +559,5 @@ if __name__ == '__main__':
     one_months = datetime.timedelta(days=3)
     tm_client.complete_trade_high_low(now.trades)
     print(now.forex_trades)
-    with open("./cached_trade_data.pkl", "wb") as f:
+    with open("../cached_data/cached_trade_data.pkl", "wb") as f:
         pickle.dump(now, f)
