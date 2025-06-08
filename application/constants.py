459b4a86c2fe18744a1cd3ae0d1a706abf7a4204
Jean Paul Cardenas <jeanpaul9712@gmail.com>
2025-06-08 20:53:33 +0200

    refactored: global restructured of files

--- application/constants.py
+++ application/constants.py
@@ -1,6 +1,31 @@
-import os
 from dotenv import load_dotenv
 import plotly.express.colors as pxc
+import logging
+import os
+
+# Get the root path dynamically, relative to this config.py file
+
+DEBUG = False
+
+
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        logger.setLevel(logging.DEBUG if DEBUG else logging.INFO)
+
+        formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+
+        file_handler = logging.FileHandler('./test.log', mode='w')
+        file_handler.setFormatter(formatter)
+
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+
+    return logger
+
 
 load_dotenv()
 
