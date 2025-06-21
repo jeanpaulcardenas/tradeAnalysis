6c5025c1539fb02317c9f785e30f7d3c5dfbe836
Jean Paul Cardenas <jeanpaul9712@gmail.com>
2025-06-21 14:29:11 +0200

    hover_template updated for all classes

--- dash_graph_f/income.py
+++ dash_graph_f/income.py
@@ -16,41 +16,19 @@ class ScatterGraph:
         self._df = self.metrics_obj.df
         self._subplots_choice = subplots_choice
         self._fig = go.Figure()
+        self._currency_symbol = self.metrics_obj.currency_symbol if self.measure == 'profit' else ''
         self.fig.update_layout(self._layout(title))
-
-    @property
-    def metrics_obj(self) -> Metrics:
-        """Returns metrics object."""
-        return self._metrics_obj
-
-    @property
-    def df(self) -> pd.DataFrame:
-        """Returns dataframe used in the plotting."""
-        return self._df
-
-    @property
-    def subplots_choice(self) -> str:
-        """Returns subplots_choice selection."""
-        return self._subplots_choice
-
-    @property
-    def fig(self) -> go.Figure:
-        """Returns figure."""
-        return self._fig
-
-    @property
-    def measure(self) -> str:
-        """Returns measure type, it will be 'pips' if pips argument is true, else 'profit'"""
-        return self._measure
 
     def get_figure(self) -> go.Figure:
         """Returns a scatter plot figure. Plots are dependent on self.subplots_choice"""
         objs = self._create_dataframes(subplots_choice=self.subplots_choice)
         for i, df in enumerate(objs):
             name = self._get_legend_name(df=df)
-            x = [df.open_time[0]] + df.close_time.to_list()  # adding open_time[0] value at index 0
-            y = [0] + df[self.measure].cumsum().to_list()    # adding a zero value at index 0
-            self._add_scatter_plot(name=name, x=x, y=y, idx=i, mode='lines+markers')
+            x = df.close_time  # adding open_time[0] value at index 0
+            y = df[self.measure].cumsum()  # adding a zero value at index 0
+            self._add_scatter_plot(
+                name=name, x=x, y=y, idx=i, mode='lines+markers',
+                hover_template=self.hover_template, custom_data=df[['order', self.measure]])
             self._add_final_markers(idx=i, name=name)  # add text marker at the end of each line plot
         return self.fig
 
@@ -78,10 +56,11 @@ class ScatterGraph:
                     **kwargs)
 
     def _create_dataframes(self, subplots_choice: str) -> list[pd.DataFrame]:
-        """returns dataframes for the given subplots_choice. e.g. if subplots_choice == pairs, returns a dataframe for each unique pair
-        containing only data with that pair symbol."""
+        """returns dataframes for the given subplots_choice. e.g. if subplots_choice == pairs, returns a dataframe
+        for each unique pair containing only data with that pair symbol."""
         try:
-            if subplots_choice == 0:  # if there is no subplots_choice return self.df, this will lead a global income figure
+            if subplots_choice == 0:
+                # if there is no subplots_choice return self.df, this will lead a global income figure
                 return [self.df]
             elif subplots_choice in _METRICS_DF_KEYS:
                 # Create a list a dataframes, one for each unique self.df[subplots_choice],
@@ -119,7 +98,8 @@ class ScatterGraph:
             legendgroup=name,
         ))
 
-    def _add_scatter_plot(self, name: str, x: list[float], y: list[float], idx: int, mode) -> None:
+    def _add_scatter_plot(self, name: str, x: list[float], y: list[float],
+                          idx: int, mode, custom_data: list[list[float]] = None, hover_template: str = None) -> None:
         """Adds scatter plot to fig."""
         self.fig.add_trace(go.Scatter(
             name=name,
@@ -128,6 +108,8 @@ class ScatterGraph:
             x=x,
             y=y,
             mode=mode,
+            customdata=custom_data,
+            hovertemplate=hover_template,
             textposition='top right',
             texttemplate='%{y}',
             line=dict(
@@ -136,8 +118,50 @@ class ScatterGraph:
                 color=_PLOTLY_GRAPH_COLORS[idx]
             )))
 
+    @property
+    def hover_template(self) -> str:
+        return '<b>Cumulative profit:</b> %{y}<br>' \
+               '<b>Order:</b> %{customdata[0]}<br>' \
+               '<b>Close date:</b> %{x}<br>' \
+               f'<b>Profit:</b> %{{customdata[1]}} {self.currency_symbol}'
+
+    @property
+    def metrics_obj(self) -> Metrics:
+        """Returns metrics object."""
+        return self._metrics_obj
+
+    @property
+    def fig(self) -> go.Figure:
+        """Returns figure."""
+        return self._fig
+
+    @property
+    def measure(self) -> str:
+        """Returns measure type, it will be 'pips' if pips argument is true, else 'profit'"""
+        return self._measure
+
+    @property
+    def df(self) -> pd.DataFrame:
+        """Returns dataframe used in the plotting."""
+        return self._df
+
+    @property
+    def subplots_choice(self) -> str:
+        """Returns subplots_choice selection."""
+        return self._subplots_choice
+
+    @property
+    def currency_symbol(self) -> str | None:
+        """Returns currency symbol for y values in the plot figure. If measure is PIPS then None is returned"""
+        return self._currency_symbol
+
 
 class TimeOpenIncome(ScatterGraph):
+    _HOVER_TEMPLATE = '<b>Profit:</b> %{y}<br>' \
+                      '<b>Time open:</b> %{x}<br>' \
+                      '<b>Order:</b> %{customdata[0]}<br>' \
+                      '<b>Date:</b> %{customdata[1]}'
+
     def __init__(self, metrics_obj, subplots_choice, pips, title, ceiling: int, denominator: int, period: str):
         super(TimeOpenIncome, self).__init__(metrics_obj, subplots_choice, pips, title)
         self.ceiling = ceiling
@@ -145,14 +169,15 @@ class TimeOpenIncome(ScatterGraph):
         self.period = period
 
     def get_figure(self) -> go.Figure:
-        """Returns a scatter plot figure. Plots are dependant on self.subplots_choice"""
+        """Returns a scatter plot figure. Plots are dependent on self.subplots_choice"""
         objs = self._create_dataframes(subplots_choice=self.subplots_choice)
         for i, df in enumerate(objs):
             name = self._get_legend_name(df=df)
             df = self._filter_by_style(df)
-            x = self._get_x_values(df)
-            y = self._get_y_values(df)
-            self._add_scatter_plot(name=name, x=x, y=y, idx=i, mode='markers')
+            x, y = self._get_x_values(df), self._get_y_values(df)
+            self._add_scatter_plot(name=name, x=x, y=y, idx=i,
+                                   mode='markers', hover_template=TimeOpenIncome._HOVER_TEMPLATE,
+                                   custom_data=df[['order', 'close_time']])
         self._update_axes()
 
         return self.fig
@@ -160,7 +185,7 @@ class TimeOpenIncome(ScatterGraph):
     def _get_x_values(self, df) -> list[float]:
         """Gets total seconds of 'delta_time' column of df and divides it by 'self.denominator'."""
         logger.info(self.denominator)
-        series = df['delta_time'].dt.total_seconds()/self.denominator
+        series = df['delta_time'].dt.total_seconds() / self.denominator
         series = series.apply(lambda x: round(x, 1))
         return series.to_list()
 
@@ -291,7 +316,7 @@ class SunBurst:
                           color_discrete_sequence=color_map,
                           )
         fig.update_traces(
-            hovertemplate=f'<b>%{{label}}</b><br>Profit: %{{value}} {self.metric.currency_symbol}<extra></extra>'
+            hovertemplate=f'<b>%{{label}}</b><br>Profit: %{{value:,d}} {self.metric.currency_symbol}'
         )
         layout = SunBurst.update_layout()
         fig.update_layout(layout)
